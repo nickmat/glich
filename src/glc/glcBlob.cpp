8966acb77fdbe6b21e577eebc82ef0ccebb2f296
Nick Matthews <nick@kizzyco.com>
2025-03-11 11:33:47 +0000

    Add source files for proposed blob value class.

---
+++ src/glc/glcBlob.cpp
@@ -0,0 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        src/glc/glcBlob.h
+ * Project:     Glich: Extendable Script Language.
+ * Purpose:     Object class to handle script blobs.
+ * Author:      Nick Matthews
+ * Website:     https://github.com/nickmat/glich
+ * Created:     11th March 2025
+ * Copyright:   Copyright (c) 2025, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  Glich is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Glich is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Glich.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
+
+#include "glcBlob.h"
+
+using namespace glich;
+using std::string;
+using std::vector;
+
+
+
+// End of src/glc/glcBlob.cpp
---
+++ src/glc/glcBlob.h
@@ -0,0 +1,39 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        src/glc/glcBlob.h
+ * Project:     Glich: Extendable Script Language.
+ * Purpose:     Object class to handle script blobs.
+ * Author:      Nick Matthews
+ * Website:     https://github.com/nickmat/glich
+ * Created:     11th March 2025
+ * Copyright:   Copyright (c) 2025, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  Glich is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Glich is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Glich.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
+
+#ifndef SRC_GLC_GLCBLOB_H_GUARD
+#define SRC_GLC_GLCBLOB_H_GUARD
+
+#include <glc/glcDefs.h>
+#include "glcValue.h"
+
+namespace glich {
+
+    
+}
+
+#endif // SRC_GLC_GLCBLOB_H_GUARD
