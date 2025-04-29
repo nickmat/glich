86601f8b3a35ae8a77ffce0259038451baae2922
Nick Matthews <nick@kizzyco.com>
2025-04-29 17:55:34 +0100

    hic/hicJulian.cpp include headers have wrong capitalization.

--- src/hic/hicJulian.cpp
+++ src/hic/hicJulian.cpp
@@ -5,7 +5,7 @@
  * Author:      Nick Matthews
  * Website:     https://github.com/nickmat/glich
  * Created:     17th March 2023
- * Copyright:   Copyright (c) 2023..2024, Nick Matthews.
+ * Copyright:   Copyright (c) 2023..2025, Nick Matthews.
  * Licence:     GNU GPLv3
  *
  *  Glich is free software: you can redistribute it and/or modify
@@ -28,8 +28,8 @@
 #include "hicJulian.h"
 
 #include "glcHelper.h"
-#include "hicmath.h"
-#include "hicrecord.h"
+#include "hicMath.h"
+#include "hicRecord.h"
 
 #include <cassert>
 
