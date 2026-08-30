14a307d051450040f0d02b98f266b233aac4bd6a
Nick Matthews <nick@kizzyco.com>
2026-08-30 01:55:29 +0100

    Change in operation of division with '/' operator.
    If at lest one side is float, result is float. If both sides are integers, result is number. Anything else is an error.
    
    Update test scripts useing '/' to '/.' float division. Add new scripts to test changed '/' operator.

--- src/glc/glcValue.cpp
+++ src/glc/glcValue.cpp
@@ -978,93 +978,11 @@ void SValue::divide( const SValue& value )
     if( propagate_error( value ) ) {
         return;
     }
-    const char* only_ints_err = "Can only divide numeric values.";
-    double left = 0.0;
-    double right = 0.0;
-    switch( type() )
-    {
-    case Type::Number:
-        switch( value.type() )
-        {
-        case Type::Number: 
-            left = static_cast<double>(get_number());
-            right = static_cast<double>(value.get_number());
-            break;
-        case Type::field:
-            left = static_cast<double>(get_number());
-            right = value.get_field_as_float();
-            break;
-        case Type::Float:
-            left = static_cast<double>(get_number());
-            right = value.get_float();
-            break;
-        default:
-            set_error( only_ints_err );
-            return;
-        }
-        break;
-    case Type::field:
-        switch( value.type() )
-        {
-        case Type::Number:
-            left = get_field_as_float();
-            right = static_cast<double>(value.get_number());
-            break;
-        case Type::field:
-            left = get_field_as_float();
-            right = value.get_field_as_float();
-            break;
-        case Type::Float:
-            left = get_field_as_float();
-            right = value.get_float();
-            break;
-        default:
-            set_error( only_ints_err );
-            return;
-        }
-        break;
-    case Type::Float:
-        switch( value.type() )
-        {
-        case Type::Number:
-            left = get_float();
-            right = static_cast<double>(value.get_number());
-            break;
-        case Type::field:
-            left = get_float();
-            right = value.get_field_as_float();
-            break;
-        case Type::Float:
-            left = get_float();
-            right = value.get_float();
-            break;
-        default:
-            set_error( only_ints_err );
-            return;
-        }
-        break;
-    default:
-        set_error( only_ints_err );
+    if( type() == Type::Float || value.type() == Type::Float ) {
+        float_div( value );
         return;
     }
-    if( std::isnan( right ) ) {
-        set_error( "Division by nan." );
-        return;
-    }
-    if( std::isinf( right ) ) {
-        string sign = (right > 0) ? "+" : "-";
-        set_error( "Division by " + sign + "inf." );
-        return;
-    }
-    if( is_zero( right ) ) {
-        set_error( "Division by zero." );
-        return;
-    }
-    if( std::isnan( left ) ) {
-        set_error( "Cannot divide nan." );
-        return;
-    }
-    set_float( left / right );
+    int_div( value );
 }
 
 void glich::SValue::int_div( const SValue& value )
